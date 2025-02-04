8d4de8bda3dfc54340d1cd13d73ebf2f6147cd92
./trenches <romanvagner@mail.com>
2025-02-04 12:20:14 +0100

    Fixed some bugs

--- src/Components/MainContainer/MainContainer.jsx
+++ src/Components/MainContainer/MainContainer.jsx
@@ -1,19 +1,11 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './MainContainer.css';
 import { Progress, Modal, Input } from 'antd';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { HabitContext } from '../Contexts/HabitContext';
 
 const MainContainer = () => {
-  const [habits, setHabits] = useState(() => {
-    const savedHabits = localStorage.getItem('habits');
-    return savedHabits ? JSON.parse(savedHabits) : [
-      { title: 'Code', timeSpent: 0, totalTime: 60, icon: 'https://www.svgrepo.com/show/473140/code.svg' },
-      { title: 'Read Books', timeSpent: 0, totalTime: 30, icon: 'https://www.svgrepo.com/show/496860/book.svg' },
-      { title: 'Practice Yoga', timeSpent: 0, totalTime: 15, icon: 'https://cdn-icons-png.flaticon.com/512/3773/3773928.png' },
-      { title: 'Take A Break From The Phone', timeSpent: 0, totalTime: 60, icon: 'https://www.svgrepo.com/show/359247/smartphone-noaccess.svg' },
-      { title: 'Run', timeSpent: 0, totalTime: 30, icon: 'https://www.svgrepo.com/show/189314/running-run.svg' },
-    ];
-  });
+  const { habits, setHabits } = useContext(HabitContext);
 
   useEffect(() => {
     localStorage.setItem('habits', JSON.stringify(habits))
@@ -81,7 +73,7 @@ const MainContainer = () => {
                 <div className="progress-bar">
                   <Progress 
                     strokeLinecap="butt" 
-                    percent={(habit.timeSpent / habit.totalTime).toFixed(2) * 100} 
+                    percent={((habit.timeSpent / habit.totalTime) * 100).toFixed(0)}
                     strokeWidth={4} 
                     size={[300, 5]} 
                   />
