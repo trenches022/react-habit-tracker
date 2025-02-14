aa96ab9635befac4b093a5a276343992fb7ec30c
./trenches <romanvagner@mail.com>
2025-02-14 17:03:45 +0100

    Dark mode in progress...

--- src/Components/Settings/Settings.jsx
+++ src/Components/Settings/Settings.jsx
@@ -5,7 +5,7 @@ const Settings = () => {
     <div className="settings-container">
       <h1>Settings</h1>
       <div className="setting-dark-mode">
-        <h3>Dark Mode</h3>
+        <h3>Dark Mode (In progress...)</h3>
         <button className="theme-switch-btn">Switch Theme</button>
       </div>
     </div>
